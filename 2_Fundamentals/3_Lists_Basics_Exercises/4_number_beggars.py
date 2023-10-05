79dd5e3d65eb9a34214f13231e05e9589eefe82c
Sibel Ayvazova <sibelayvazova@gmail.com>
2023-10-06 00:00:35 +0300

    Updated 4_number_beggars.py

--- 2_Fundamentals/3_Lists_Basics_Exercises/4_number_beggars.py
+++ 2_Fundamentals/3_Lists_Basics_Exercises/4_number_beggars.py
@@ -1,17 +1,13 @@
 string = input()
-list_of_integers = list(map(int, string.split(", ")))
+list_money = list(map(int, string.split(", ")))
 count_beggars = int(input())
 sum_list = []
 
 
 for index1 in range(count_beggars):
-
-    if index1 + 1 > len(list_of_integers):
-        total_sum = 0
-    else:
-        total_sum = list_of_integers[index1]
-        for index2 in range(index1 + count_beggars, len(list_of_integers), count_beggars):
-            total_sum += list_of_integers[index2]
+    total_sum = 0
+    for index2 in range(index1, len(list_money), count_beggars):
+        total_sum += list_money[index2]
 
     sum_list.append(total_sum)
 
