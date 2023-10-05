86e67d4ec8ace738be2bdff2165a472feaf43d10
Sibel Ayvazova <sibelayvazova@gmail.com>
2023-10-05 22:58:34 +0300

    Updated 3_football_cards.py

--- 2_Fundamentals/3_Lists_Basics_Exercises/3_football_cards.py
+++ 2_Fundamentals/3_Lists_Basics_Exercises/3_football_cards.py
@@ -1,25 +1,17 @@
-Team_A = list(range(1,12))
-Team_B = list(range(1,12))
+Team_A = ['A-1', 'A-2', 'A-3', 'A-4', 'A-5', 'A-6', 'A-7', 'A-8', 'A-9', 'A-10', 'A-11']
+Team_B = ['B-1', 'B-2', 'B-3', 'B-4', 'B-5', 'B-6', 'B-7', 'B-8', 'B-9', 'B-10', 'B-11']
 
 cards = input()
-list_cards = cards.split(" ")
+list_cards = cards.split()
 
 terminated = False
 for card in list_cards:
 
-    elements_card = card.split("-")
+    if card in Team_A:
+        Team_A.remove(card)
 
-    if elements_card[0] == "A":
-        number_player = int(elements_card[1])
-        if number_player not in Team_A:
-            continue
-        Team_A.remove(int(elements_card[1]))
-
-    elif elements_card[0] == "B":
-        number_player = int(elements_card[1])
-        if number_player not in Team_B:
-            continue
-        Team_B.remove(int(elements_card[1]))
+    elif card in Team_B:
+        Team_B.remove(card)
 
     if len(Team_A) < 7 or len(Team_B) < 7:
         terminated = True
@@ -28,6 +20,3 @@ for card in list_cards:
 print(f"Team A - {len(Team_A)}; Team B - {len(Team_B)}")
 if terminated:
     print("Game was terminated")
-
-
-
