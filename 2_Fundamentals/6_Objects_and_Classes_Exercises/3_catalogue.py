1d04c8750f288b7b2cfbcf781b2617054408a7ba
Sibel Ayvazova <sibelayvazova@gmail.com>
2023-11-03 22:41:56 +0200

    Added exercises to 6_Objects_and _Classes-Exercises

---
+++ 2_Fundamentals/6_Objects_and_Classes_Exercises/3_catalogue.py
@@ -0,0 +1,27 @@
+class Catalogue:
+    def __init__(self, name: str):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        list_by_letter = [item for item in self.products if first_letter.upper() == item[0].upper()]
+        return list_by_letter
+
+    def __repr__(self):
+        sorted_list = sorted(self.products)
+        nl = '\n'
+        return f'Items in the {self.name} catalogue:\n{nl.join(sorted_list)}'
+
+
+catalogue = Catalogue("Furniture")
+
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
